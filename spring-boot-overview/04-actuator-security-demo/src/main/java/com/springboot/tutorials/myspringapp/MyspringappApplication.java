9f79d087e292af6811faaa85a8e2ef4f06b182d3
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-07-25 17:45:30 +0530

    create 04-actuator-security-demo for learning about how to secure endpoints

---
+++ spring-boot-overview/04-actuator-security-demo/src/main/java/com/springboot/tutorials/myspringapp/MyspringappApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.tutorials.myspringapp;
+
+// 03-actuator-demo
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringappApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MyspringappApplication.class, args);
+	}
+
+}
