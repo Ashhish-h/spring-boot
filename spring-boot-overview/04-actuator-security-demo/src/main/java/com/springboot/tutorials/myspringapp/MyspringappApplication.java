b26ca4f782908ea28614587cbd3fb183c6b225ec
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-07-25 18:30:15 +0530

    add comments and remove comments

--- spring-boot-overview/04-actuator-security-demo/src/main/java/com/springboot/tutorials/myspringapp/MyspringappApplication.java
+++ spring-boot-overview/04-actuator-security-demo/src/main/java/com/springboot/tutorials/myspringapp/MyspringappApplication.java
@@ -1,6 +1,5 @@
 package com.springboot.tutorials.myspringapp;
 
-// 03-actuator-demo
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
