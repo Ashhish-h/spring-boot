c6089cd158657f5cdc21ed43d1632ba808708d19
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-07-26 20:42:39 +0530

    get the custom properties using @Value Annotation and add an endpoint to show the properties on the web.

--- spring-boot-overview/06-properties-demo/src/main/java/com/springboot/tutorials/myspringapp/rest/FunRestController.java
+++ spring-boot-overview/06-properties-demo/src/main/java/com/springboot/tutorials/myspringapp/rest/FunRestController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FunRestController {
     // create a hello world function
-
     /*
         @GetMapping annotation is used for mapping HTTP GET requests onto specific handler methods.
         It is composed annotation that acts as a shortcut for @RequestMapping(method=RequestMethod.
@@ -26,5 +25,29 @@ public class FunRestController {
     @GetMapping("/information")
     public String getInfo(){
         return "This is a demo project only for learning purpose";
+    }
+
+    /*
+        Creating variables and assigning them the custom properties declared in application.properties file.
+        Thats the main properties file of the spring boot it used for cconfigurations and descriping custom properties.
+        W'll also create a endpoint to show those properties on the web.
+     */
+
+    @Value("${project.name}")
+    private String projectNname;
+
+    @Value("${project.team.leader}")
+    private String teamLeader;
+
+    @Value("${project.platform}")
+    private String platform;
+
+    @Value("${project.idea}")
+    private String projectIdea;
+
+    @GetMapping("/project-info")
+    public String projectInfo(){
+        return "project name : " + projectNname + "project team leader : " + teamLeader + "project's platform : "
+                + platform + "project idea : " + projectIdea;
     }
 }
