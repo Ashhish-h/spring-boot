b300575682a97e0f8ca955fc5b41e3bae20a47e5
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-07-28 20:08:03 +0530

    create an interface and a bean class to implement the interface.

---
+++ spring-boot-spring-core/01-constructor-injection/src/main/java/com/spring_core_overview/springcoredemo/BadmintonCoach.java
@@ -0,0 +1,19 @@
+package com.spring_core_overview.springcoredemo;
+
+import org.springframework.stereotype.Component;
+
+// This @Component annotation marks the class as spring beans class
+@Component
+public class BadmintonCoach implements Coach{
+
+
+    @Override
+    public String dailyWorkOut() {
+        return " You are required to do daily workout for the fitness of the body ";
+    }
+
+    @Override
+    public String dailyPractice() {
+        return " You are required to do daily practice so that you can improve your performance ";
+    }
+}
