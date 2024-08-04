c9616ee8dd55ec495e88983750ca1968859cb266
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-04 20:23:53 +0530

    add comments for more information

--- spring-boot-spring-core/01-constructor-injection/src/main/java/com/spring_core_overview/springcoredemo/SpringcoredemoApplication.java
+++ spring-boot-spring-core/01-constructor-injection/src/main/java/com/spring_core_overview/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,10 @@ package com.spring_core_overview.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	By default, @SpringBootApplication search for the dependencies, but within the same class to make it search outside
+	the default we need to use @SpringBootApplication(scanBasePackage="package name")
+ */
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
