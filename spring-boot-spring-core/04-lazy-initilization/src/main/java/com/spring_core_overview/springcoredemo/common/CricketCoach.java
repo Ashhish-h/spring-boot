96c44960f0e6a73d196668267ae8b1453d9e9e61
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-06 16:35:44 +0530

    mark bean classes as lazy so that they won't get automatically initialized

--- spring-boot-spring-core/04-lazy-initilization/src/main/java/com/spring_core_overview/springcoredemo/common/CricketCoach.java
+++ spring-boot-spring-core/04-lazy-initilization/src/main/java/com/spring_core_overview/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,11 @@
 package com.spring_core_overview.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+// if we mark these bean classes as lazy they won't be initialized, they will only be initialized when they are needed
+@Lazy
 public class CricketCoach implements Coach{
     public CricketCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
