ab34d0e31710d9f3fd4f1bc6b7cb2afc4e2058de
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-06 16:39:02 +0530

    add method of how to declare all classes as lazy using application.properties file

--- spring-boot-spring-core/04-lazy-initilization/src/main/java/com/spring_core_overview/springcoredemo/common/TTCoach.java
+++ spring-boot-spring-core/04-lazy-initilization/src/main/java/com/spring_core_overview/springcoredemo/common/TTCoach.java
@@ -1,8 +1,11 @@
 package com.spring_core_overview.springcoredemo.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+// if we mark these bean classes as lazy they won't be initialized, they will only be initialized when they are needed
+@Lazy
 public class TTCoach implements Coach{
     public TTCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
