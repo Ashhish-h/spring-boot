4b72b10f60b09766626c1e5adf10b2bbbaf4b560
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-08 19:31:52 +0530

    remove comments and annotation from previous project, verified default scope of the bean

--- spring-boot-spring-core/05-bean-scopes/src/main/java/com/spring_core_overview/springcoredemo/common/CricketCoach.java
+++ spring-boot-spring-core/05-bean-scopes/src/main/java/com/spring_core_overview/springcoredemo/common/CricketCoach.java
@@ -4,12 +4,6 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
-// if we mark these bean classes as lazy they won't be initialized, they will only be initialized when they are needed
-/*
-    to avoid marking every class as lazy which is troublesome we can globally declare all the classes as lazy by editing
-    the application.properties file by adding " spring.main.lazy-initialization=true "
- */
-@Lazy
 public class CricketCoach implements Coach{
     public CricketCoach(){
         System.out.println("In constructor: " + getClass().getSimpleName());
