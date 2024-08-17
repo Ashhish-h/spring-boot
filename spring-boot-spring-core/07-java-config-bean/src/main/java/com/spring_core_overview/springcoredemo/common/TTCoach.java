89d9e15eae59c70123917f84d95e4fd86c43dee3
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-08-17 13:03:17 +0530

    create 07-java-config-bean to learn about why using bean annotations is important

---
+++ spring-boot-spring-core/07-java-config-bean/src/main/java/com/spring_core_overview/springcoredemo/common/TTCoach.java
@@ -0,0 +1,20 @@
+package com.spring_core_overview.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TTCoach implements Coach{
+    public TTCoach(){
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+    @Override
+    public String dailyWorkOut() {
+        return " Do daily flexibility workout ";
+    }
+
+    @Override
+    public String dailyPractice() {
+        return " Do daily practice";
+    }
+}
