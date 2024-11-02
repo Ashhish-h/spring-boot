27f963e47e5665b3edc36db5b355532eb820cb79
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-11-02 17:13:12 +0530

    add 01-crudoperationsdemo to learn about crud operations.

---
+++ spring-boot-hibernate-jpa-crud/01-crudoperationsdemo/src/test/java/com/springboot/crudoperationsdemo/CrudoperationsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.crudoperationsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudoperationsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
