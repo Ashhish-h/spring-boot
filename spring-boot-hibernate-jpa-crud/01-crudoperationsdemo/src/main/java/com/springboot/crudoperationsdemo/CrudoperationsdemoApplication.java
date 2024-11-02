2dfed7d9ce1ea2f1767d30fb35ef04a3baa6d1f1
Ashhish-h <kushwaha.ashish2611@gmail.com>
2024-11-02 18:59:13 +0530

    add a lambda function runner for command line and add database configuration in the application.properties file and also remove the spring banner and set the logging to warn

--- spring-boot-hibernate-jpa-crud/01-crudoperationsdemo/src/main/java/com/springboot/crudoperationsdemo/CrudoperationsdemoApplication.java
+++ spring-boot-hibernate-jpa-crud/01-crudoperationsdemo/src/main/java/com/springboot/crudoperationsdemo/CrudoperationsdemoApplication.java
@@ -1,7 +1,9 @@
 package com.springboot.crudoperationsdemo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudoperationsdemoApplication {
@@ -10,4 +12,17 @@ public class CrudoperationsdemoApplication {
 		SpringApplication.run(CrudoperationsdemoApplication.class, args);
 	}
 
+
+	/*
+		 This CommandLineRunner is form spring boot library and this method will only execute after all the beans are
+		 loaded. We are going command line to actually understand how crud operations work. Now to connect to the database
+		 add the configuration in the application.properties file
+	 */
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
 }
