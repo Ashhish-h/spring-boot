62d668f2de5a0b471dc62f5352cb78d6a84f1c1b
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-26 14:14:55 +0530

    add mappings in UserController

--- spring-security/spring-security/src/main/java/com/spring_security/spring_security/controller/UserController.java
+++ spring-security/spring-security/src/main/java/com/spring_security/spring_security/controller/UserController.java
@@ -1,4 +1,32 @@
 package com.spring_security.spring_security.controller;
 
+
+import com.spring_security.spring_security.entity.Users;
+import com.spring_security.spring_security.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/")
+    public List<Users> getAllUsers(){
+        return this.userService.getUsers();
+    }
+
+    @GetMapping("/{username}")
+    public Users getSingleUser(@PathVariable("username") String username){
+        return this.userService.getSingleUser(username);
+    }
+
+    @PostMapping("/addUser")
+    public Users addUser(@RequestBody Users user){
+        return this.userService.addUser(user);
+    }
+
 }
