94262fea94f43557872f7d9adfbe1ffcb049cd20
Ashhish-h <kushwaha.ashish2611@gmail.com>
2025-03-26 13:23:11 +0530

    add schema of users and business logic in userService

--- spring-security/spring-security/src/main/java/com/spring_security/spring_security/service/UserService.java
+++ spring-security/spring-security/src/main/java/com/spring_security/spring_security/service/UserService.java
@@ -1,5 +1,37 @@
 package com.spring_security.spring_security.service;
 
+
+import com.spring_security.spring_security.entity.Users;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserService {
+
+    List<Users> users = new ArrayList<>();
+
+    public UserService() {
+        users.add(new Users("ashish", "ashish123", "ashish@gmail.com"));
+        users.add(new Users("abhishek", "abhishek123", "abhishek@gmail.com"));
+        users.add(new Users("sachin", "sachin123", "sachin@gmail.com"));
+    }
+
+    // to get all users
+    public List<Users> getUsers() {
+        return this.users;
+    }
+
+    // get a single user
+    public Users getSingleUser(String username){
+        return this.users.stream().filter((users) -> users.getUsername().equals(username)).findAny().orElse(null);
+    }
+
+    // add a user
+    public Users addUser(Users user) {
+        this.users.add(user);
+        return user;
+    }
+
 
 }
